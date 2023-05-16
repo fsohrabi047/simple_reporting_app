deb6af261e56f05f25c41a725d4bab75f209aa8e
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-16 22:24:05 +0330

    added custom serializer

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,13 +7,17 @@ import {
   Delete,
   Param,
   Query,
+  UseInterceptors,
   NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from "../interceptors/serialize.interceptor";
+import { UserDto } from "./dtos/user.dto";
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
