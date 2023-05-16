8cba4513834b976e414db968678b8cb1f32ba460
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-16 18:24:05 +0330

    initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { CreateUserDto } from "./dtos/create-user.dto";
+
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
